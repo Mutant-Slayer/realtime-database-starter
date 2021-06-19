ad0a4564589e2dcaf08a48aecd7df86c9b940d91
Mutant-Slayer <72788845+Mutant-Slayer@users.noreply.github.com>
2021-06-19 12:19:54 +0530

    Add files via upload

---
+++ src/utils/config.js
@@ -0,0 +1,19 @@
+//TODO:DONE add firebase configuration and export it
+export const firebaseConfig = {
+    apiKey: "AIzaSyABNTVUtP67Jv1DawGDUvCrnVCbK7mLhic",
+    authDomain: "mygitapp-1c650.firebaseapp.com",
+    databaseURL: "https://mygitapp-1c650.firebaseio.com",
+    projectId: "mygitapp-1c650",
+    storageBucket: "mygitapp-1c650.appspot.com",
+    messagingSenderId: "728523829891",
+    appId: "1:728523829891:web:7f7be0bf2093697353e1b6",
+    measurementId: "G-3RGFX0QP2W"
+  };
+
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
